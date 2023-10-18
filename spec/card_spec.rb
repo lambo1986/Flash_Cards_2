d4717cfbe6e45b114db87eb5353d80b328e8a16b
Nathan Lambertson <lambertones@gmail.com>
2023-10-18 15:11:20 -0500

    fix the tests

---
+++ spec/card_spec.rb
@@ -0,0 +1,28 @@
+require 'pry'
+require './lib/card'
+
+RSpec.describe Card do
+  it "exists" do
+    card = Card.new("What is the capitol of Alaska?", "Juneau", :Geography)
+
+    expect(card).to be_instance_of(Card)
+  end
+
+  it "has a question" do
+    card = Card.new("What is the capitol of Alaska?", "Juneau", :Geography)
+
+    expect(card.question).to eq("What is the capitol of Alaska?")
+  end
+
+  it "has an answer" do
+    card = Card.new("What is the capitol of Alaska?", "Juneau", :Geography)
+
+    expect(card.answer).to eq("Juneau")
+  end
+
+  it "has a category" do
+    card = Card.new("What is the capitol of Alaska?", "Juneau", :Geography)
+
+    expect(card.category).to eq(:Geography)
+  end
+end
